84826870c9904d5fe4a3d62cb4835651b4dfb1bf
rocpjunior <jugatenho@hotmail.com>
2025-06-20 20:16:42 -0300

    feat: criado layout da tela de Detalhes de Produtos

---
+++ app/src/main/java/com/rocpjunior/desafio_git_e_github_e5_ifood/detalhes_produtos.kt
@@ -0,0 +1,13 @@
+package com.rocpjunior.desafio_git_e_github_e5_ifood
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class detalhes_produtos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_detalhes_produtos)
+
+    }
+}
